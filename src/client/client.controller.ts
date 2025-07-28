2ce59fc1da0745dc4bd01f83d7bf879bb8c29f83
natan <natanbarbosax193@gmail.com>
2025-07-28 13:46:19 -0300

    edit: add create client endpoint

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ClientService } from './client.service';
+import { CreateClientDto } from './client.dto';
 
 @Controller('client')
 export class ClientController {
   constructor(private readonly clientService: ClientService) {}
+
+  @Post()
+  async create(@Body() dto: CreateClientDto) {
+    return await this.clientService.create(dto);
+  }
 }
