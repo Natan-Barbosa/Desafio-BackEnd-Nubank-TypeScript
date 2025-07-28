5ce29ded65af8ce9882143c350360c73b8a7ccc9
natan <natanbarbosax193@gmail.com>
2025-07-28 16:56:08 -0300

    edit: add validation before add a  new contact and dto to saved contact

--- src/contacts/contacts.service.ts
+++ src/contacts/contacts.service.ts
@@ -2,7 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { ContactEntity } from './contact.entity';
-import { CreateContactDto } from './contacts.dto';
+import { CreateContactDto, GetContactDto } from './contacts.dto';
 import { plainToInstance } from 'class-transformer';
 import { ClientEntitie } from 'src/client/client.entity';
 
@@ -20,6 +20,13 @@ export class ContactsService {
     const clientExists = await this.clientRepository.findOneBy({
       id: dto.clientID,
     });
+    const contactExists = await this.contactsRepository.existsBy({
+      contactNumber: dto.contactNumber,
+    });
+
+    if (contactExists) {
+      throw new BadRequestException('The Informed Contact Already Exists!');
+    }
     if (!clientExists) {
       throw new BadRequestException(
         'Client Not Exists, Please Inform A Valid Client',
@@ -28,8 +35,10 @@ export class ContactsService {
 
     const newContact = plainToInstance(ContactEntity, {
       ...dto,
+      client: clientExists,
       createdAt: new Date(),
     });
-    return await this.contactsRepository.save(newContact);
+    const savedContact = await this.contactsRepository.save(newContact);
+    return plainToInstance(GetContactDto, savedContact);
   }
 }
