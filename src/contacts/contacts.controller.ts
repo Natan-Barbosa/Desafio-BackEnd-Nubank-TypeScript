02866bc90bec7358ff1cf7c9562c2510bb063bff
natan <natanbarbosax193@gmail.com>
2025-07-28 16:26:56 -0300

    edit: add endpoint to create contact

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ContactsService } from './contacts.service';
+import { CreateContactDto } from './contacts.dto';
 
 @Controller('contacts')
 export class ContactsController {
   constructor(private readonly contactsService: ContactsService) {}
+
+  @Post()
+  async create(@Body() dto: CreateContactDto) {
+    return await this.contactsService.create(dto);
+  }
 }
