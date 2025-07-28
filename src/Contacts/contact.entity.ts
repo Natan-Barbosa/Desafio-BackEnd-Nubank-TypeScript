8551b8fd6d79faa058aac98023792e7c9edb7683
natan <natanbarbosax193@gmail.com>
2025-07-28 12:54:03 -0300

    add: contact entitie

---
+++ src/Contacts/contact.entity.ts
@@ -0,0 +1,20 @@
+import { ClientEntitie } from 'src/Client/client.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'contact_table' })
+export class ContactEntity {
+  @PrimaryGeneratedColumn('identity')
+  id: number;
+
+  @Column({ name: 'contact_name', nullable: false })
+  contactName: string;
+
+  @Column({ name: 'contact_number', nullable: false, unique: true })
+  contactNumber: string;
+
+  @Column({ name: 'created_at' })
+  createdAt: Date;
+
+  @ManyToOne(() => ClientEntitie, (ClientEntitie) => ClientEntitie.contacts)
+  client: ClientEntitie;
+}
