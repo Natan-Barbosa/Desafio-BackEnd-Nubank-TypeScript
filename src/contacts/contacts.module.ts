7faf59e2f864529ac375edbd06e705d70322ab42
natan <natanbarbosax193@gmail.com>
2025-07-28 16:27:26 -0300

    edit: add repositories to service

--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ContactsService } from './contacts.service';
 import { ContactsController } from './contacts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ContactEntity } from './contact.entity';
+import { ClientEntitie } from 'src/client/client.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ContactEntity, ClientEntitie])],
   controllers: [ContactsController],
   providers: [ContactsService],
 })
