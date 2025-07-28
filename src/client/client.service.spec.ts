a3eeae980b6f349e13536d39720b2dc05c8f4958
natan <natanbarbosax193@gmail.com>
2025-07-28 18:19:51 -0300

    edit: add tests cases

--- src/client/client.service.spec.ts
+++ src/client/client.service.spec.ts
@@ -1,18 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClientService } from './client.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ClientEntitie } from './client.entity';
+import { CreateClientDto } from './client.dto';
+import { plainToClass } from 'class-transformer';
+
+const mockRepository = {
+  existsBy: jest.fn(),
+  save: jest.fn(),
+};
 
 describe('ClientService', () => {
   let service: ClientService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ClientService],
+      providers: [
+        ClientService,
+        {
+          provide: getRepositoryToken(ClientEntitie),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<ClientService>(ClientService);
   });
 
+  const mockDto = new CreateClientDto();
+  mockDto.clientName = 'Fake Name';
+  mockDto.cnpj = '12345678901234';
+  mockDto.createdAt = new Date();
+
+  const mockPlain = plainToClass(ClientEntitie, {
+    ...mockDto,
+    createdAt: new Date(),
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('Should Throw Client Already Exists', async () => {
+    mockRepository.existsBy.mockResolvedValue(true);
+
+    await expect(service.create(mockDto)).rejects.toThrow(
+      'Client Already Exists',
+    );
+  });
+
+  it('Should Create With Success', async () => {
+    mockRepository.existsBy.mockResolvedValue(false);
+    mockRepository.save.mockResolvedValue(mockPlain);
+
+    const serviceResponse = await service.create(mockDto);
+
+    expect(serviceResponse).not.toBeNull();
+    expect(serviceResponse).not.toBeUndefined();
   });
 });
