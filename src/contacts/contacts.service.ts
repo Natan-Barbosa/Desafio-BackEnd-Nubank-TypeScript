f1791babd5d16b6fc76f5516ae01d4cff7e1caf8
natan <natanbarbosax193@gmail.com>
2025-07-28 16:27:11 -0300

    edit: add method to create contact

--- src/contacts/contacts.service.ts
+++ src/contacts/contacts.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ContactEntity } from './contact.entity';
+import { CreateContactDto } from './contacts.dto';
+import { plainToInstance } from 'class-transformer';
+import { ClientEntitie } from 'src/client/client.entity';
 
 @Injectable()
-export class ContactsService {}
+export class ContactsService {
+  constructor(
+    @InjectRepository(ContactEntity)
+    private contactsRepository: Repository<ContactEntity>,
+
+    @InjectRepository(ClientEntitie)
+    private clientRepository: Repository<ClientEntitie>,
+  ) {}
+
+  async create(dto: CreateContactDto) {
+    const clientExists = await this.clientRepository.findOneBy({
+      id: dto.clientID,
+    });
+    if (!clientExists) {
+      throw new BadRequestException(
+        'Client Not Exists, Please Inform A Valid Client',
+      );
+    }
+
+    const newContact = plainToInstance(ContactEntity, {
+      ...dto,
+      createdAt: new Date(),
+    });
+    return await this.contactsRepository.save(newContact);
+  }
+}
